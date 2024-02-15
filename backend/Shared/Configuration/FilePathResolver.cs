09f088102b27170309da21dae95d8df1128bade6
Georgi Kirilov <georgi.kirilov@smartit.bg>
2024-02-15 13:27:37 +0200

    Modify the FilePathResolver

--- backend/Shared/Configuration/FilePathResolver.cs
+++ backend/Shared/Configuration/FilePathResolver.cs
@@ -9,14 +9,14 @@ public sealed class FilePathResolver(IWebHostEnvironment environment)
     public string GetResourceFilePath(string fileName)
     {
         var projectName = Assembly.GetCallingAssembly().GetName().Name;
-        var basePath = environment.IsDevelopment() ? $"/src/{projectName}/Resources" : $"./{projectName}/Resources";
+        var basePath = environment.IsDevelopment() ? $"/src/{projectName}/{projectName}/Resources" : $"./{projectName}/{projectName}/Resources";
         return Path.Combine(basePath, fileName);
     }
 
     public string ResolvePath(string relativeRootPath)
     {
         var projectName = Assembly.GetCallingAssembly().GetName().Name;
-        var basePath = environment.IsDevelopment() ? $"/src/{projectName}" : $"./{projectName}";
+        var basePath = environment.IsDevelopment() ? $"/src/{projectName}/{projectName}" : $"./{projectName}/{projectName}";
         return Path.Combine(basePath, relativeRootPath);
     }
 }
